7f82bafc573a14124268687f496114ef6dcb8479
i-Mhyke <58915377+i-Mhyke@users.noreply.github.com>
2020-03-14 16:45:23 +0100

    new update

--- weatherapp.js
+++ weatherapp.js
@@ -1,14 +1,11 @@
 //https://api.weatherbit.io/v2.0/current?city=lagos, ng&key=8b832a95613a4ffa9fc7d9dce08d5593
 //http://newsapi.org/v2/top-headlines?country=NG&category=health&apiKey=7ab076ac5ea840c88bed067ebc648c30
 
-
-
 const weatherApp = ()=> {
 
     const cityForm = () => {
         const mainForm = document.querySelector("#search_item");
         
-
         mainForm.addEventListener("submit", (event) =>{
             event.preventDefault();
             const formValue = document.querySelector("#search_value").value;
@@ -17,18 +14,26 @@ const weatherApp = ()=> {
             getSearch(formValue);
         })
     }
+        const loader = document.querySelector('#loading');
+        const errorNotice = document.querySelector("#error");
+        const parent = document.querySelector("#report_container");
+        
 
-    const getSearch = (city) =>{
+        const getSearch = (city) =>{
         const url = `https://api.weatherbit.io/v2.0/current?city=${city}&key=8b832a95613a4ffa9fc7d9dce08d5593`;
-
+        
+        parent.innerHTML = '';
+        parent.insertAdjacentHTML("afterbegin", `<div id="loading" class="text-center mt-5">
+                    <img src="images/loader2.gif">
+                </div>`);
         fetch(url)
         .then((response) => {
             return response.json();
         })
         .then( (city) =>{
             let reports = city.data;
-            let parent = document.querySelector("#report_container");
             let template = '';
+
             return reports.map( (report) =>{
                 template += `
                 <div class="text-center mt-5 animated bounceInDown">
@@ -43,6 +48,7 @@ const weatherApp = ()=> {
                 `
                 parent.innerHTML = '';
                 parent.insertAdjacentHTML("afterbegin", template);
+                
 
                 anime({
                 targets: '.cloud', 
@@ -55,15 +61,20 @@ const weatherApp = ()=> {
 
                 })
             })
+            
             .catch((error) => {
+                parent.innerHTML = '';
+                parent.insertAdjacentHTML("afterbegin", `<div id="error" class="text-center mt-5">
+                    <h1 class="emoji">😕</h1>
+                    <h3>SORRY CITY NOT FOUND</h3>
+                </div>`);
                 console.error('Error:', error);
-                document.querySelector("#error").insertAdjacentHTML("afterbegin", `<h3 class="emoji">😕</h3>
-                    <h4 >SORRY CITY NOT FOUND</h4>`)
+
                 anime({
                 targets: '.emoji', 
                 translateX: [-50, 50],
                 direction: 'alternate',
-                duration: 3000,
+                duration: 4000,
                 loop: true,
                 easing: 'linear'
                 });
